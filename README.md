0e87380c932a9bead88df5a4ae44c0bbe7ee134b
Jordan Castelloe <jordan.castelloe@gmail.com>
2018-02-06 10:06:23 -0600

    data fetched through postman

---
+++ README.md
@@ -0,0 +1,3 @@
+## Proxy Server for NSS Front-End Capstone
+
+This is a proxy server I built to access the [Google Places Web Service API](https://developers.google.com/places/) for my front-end capstone project for Nashville Software School. Instructions can be found [here](https://github.com/BlaiseRoberts/proxy-server/blob/master/server.js)!
---
+++ server.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 6060
+app.set('port', port)
+const request = require('request');
+const apiUrl = "https://maps.googleapis.com";
+
+// MIDDLEWARE (transform stream)
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
+app.get(`/api/*`, (req, res) => {
+  let apiCall = req.url.slice('/api/'.length)
+  let apiReq = `${apiUrl}/${apiCall}`
+  request.get(apiReq, (err, _, body) => {
+    res.send(body)
+  });
+});
+
+
+
+app.listen(port, () =>
+  console.log(`Listening on port: ${port}`)
+)
